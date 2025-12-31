fc070de8da9c656c7361bfbaa86c345cfadebae7
kingsmoovedap-design <kingsmoovedap@gmail.com>
2025-12-31 11:42:11 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,40 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  // ...existing config
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'public/index.html'
+      template: 'public/index.html',
+      inject: 'body'
     })
-  ]
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    port: 3000,
+    historyApiFallback: true,
+    proxy: {
+      '/royal': 'http://localhost:3000',
+      '/mint': 'http://localhost:3000',
+      '/simulate': 'http://localhost:3000',
+      '/health': 'http://localhost:3000'
+    }
+  }
 };
