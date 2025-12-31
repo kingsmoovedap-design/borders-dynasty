1bc98be4bb13c6e4c0baa618f06bb3a629c8f20a
kingsmoovedap-design <kingsmoovedap@gmail.com>
2025-12-31 00:30:06 -0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
-require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.18",
@@ -8,5 +9,8 @@ module.exports = {
       url: process.env.RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
