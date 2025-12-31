711c4e6fdbe2bb632a03efe572b0c6f9bb338334
kingsmoovedap-design <kingsmoovedap@gmail.com>
2025-12-31 11:40:15 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,10 @@
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.js'), // Dynamically resolves the entry file
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      }
-    ]
-  }
+  // ...existing config
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    })
+  ]
 };
