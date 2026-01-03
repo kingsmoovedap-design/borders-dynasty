b4227e2b6ab2407bdd7aab1e400b7001c263f840
kingsmoovedap-design <kingsmoovedap@gmail.com>
2026-01-03 00:53:36 -0800

    Update main.jsx

--- apps/omega/src/main.jsx
+++ apps/omega/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles.css';
+import { useEffect } from "react";
+import "./dashboard.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+  useEffect(() => {
+    if (window.init) {
+      window.init();
+    }
+  }, []);
+
+  return (
+    <div id="app"></div>
+  );
+}
