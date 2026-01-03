51acd04cdc7eaa15730f653c51e315bc491cae96
kingsmoovedap-design <kingsmoovedap@gmail.com>
2026-01-02 21:40:58 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
 import { ethers } from 'ethers';
 import BordersSovereignCoin from './abis/BordersSovereignCoin.json';
 import ScrollHashRegistry from './abis/ScrollHashRegistry.json';
