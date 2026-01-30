5259124c17f18812f6215b0f1ac9af790e63a5ab
kingsmoovedap-design <kingsmoovedap@gmail.com>
2026-01-30 02:26:20 -0800

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,7 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@openzeppelin/hardhat-upgrades";
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
-const ARBITRUM_RPC_URL = process.env.ARBITRUM_RPC_URL || "";
-const ARBITRUM_DEPLOYER_KEY = process.env.ARBITRUM_DEPLOYER_KEY || "";
-
-const config: HardhatUserConfig = {
-  solidity: "0.8.20",
-  networks: {
-    arbitrum: {
-      url: ARBITRUM_RPC_URL,
-      accounts: ARBITRUM_DEPLOYER_KEY ? [ARBITRUM_DEPLOYER_KEY] : [],
-    },
-    // add testnets if desired, e.g. arbitrumGoerli
-  },
-  etherscan: {
-    // Arbiscan API key if you want verification
-    apiKey: process.env.ARBISCAN_API_KEY || "",
-  },
-};
-
-export default config;
+networks: {
+  bsc: {
+    url: process.env.BSC_RPC_URL,
+    accounts: [process.env.PRIVATE_KEY],
+    chainId: 56
+  }
+}
