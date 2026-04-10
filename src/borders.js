d2bf03fff980a1687c9b3c3fbd2ff123f4b9c1b3
kingsmoovedap-design <kingsmoovedap@gmail.com>
2026-04-09 23:45:54 -0700

    Create borders.js

---
+++ src/borders.js
@@ -0,0 +1,9 @@
+export const metadata = {
+    name: "borders",
+    version: "1.0.0",
+    description: "Borders Dynasty plugin"
+};
+
+export function bordersCommands(args, runtime) {
+    runtime.logger.info("Borders Dynasty plugin activated.");
+}
